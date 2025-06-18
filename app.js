5af64a15be2fcbc7416447743451727d0fb76fdb
Muzammil Hussain <muzammilch2021@gmail.com>
2025-06-18 06:05:07 +0500

    first commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require("method-override");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
